0182d5c343767c4cc089d79ffde76bebcc6b4832
Julian Gomez <juliangomez.xvii@gmail.com>
2023-08-09 12:31:56 -0300

    Nuevas funcionalidades para Registrar, Editar y Eliminar un contacto

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -50,5 +50,92 @@ namespace ProyectoCrud.Controllers
                 
             return View(olista);
         }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? idContact)
+        {
+            if(idContact == null)
+            {
+                return RedirectToAction("inicio", "Contact");
+            }
+
+            Contact ocontact = olista.Where(c => c.IdContacto == idContact).FirstOrDefault();
+
+            return View(ocontact);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? idContact)
+        {
+            if (idContact == null)
+            {
+                return RedirectToAction("inicio", "Contact");
+            }
+
+            Contact ocontact = olista.Where(c => c.IdContacto == idContact).FirstOrDefault();
+
+            return View(ocontact);
+        }
+
+        [HttpPost]
+        public ActionResult Register(Contact ocontact)
+        {
+
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Registrar", oconexion);
+                cmd.Parameters.AddWithValue("Nombres", ocontact.Nombres);
+                cmd.Parameters.AddWithValue("Apellidos", ocontact.Apellidos);
+                cmd.Parameters.AddWithValue("Telefono", ocontact.Telefono);
+                cmd.Parameters.AddWithValue("Correo", ocontact.Correo);
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            return RedirectToAction("inicio", "Contact");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Contact ocontact)
+        {
+
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Editar", oconexion);
+                cmd.Parameters.AddWithValue("IdContacto", ocontact.IdContacto);
+                cmd.Parameters.AddWithValue("Nombres", ocontact.Nombres);
+                cmd.Parameters.AddWithValue("Apellidos", ocontact.Apellidos);
+                cmd.Parameters.AddWithValue("Telefono", ocontact.Telefono);
+                cmd.Parameters.AddWithValue("Correo", ocontact.Correo);
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            return RedirectToAction("inicio", "Contact");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string IdContacto)
+        {
+
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Eliminar", oconexion);
+                cmd.Parameters.AddWithValue("IdContacto", IdContacto);
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            return RedirectToAction("inicio", "Contact");
+        }
     }
 }
